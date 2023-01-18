e3172409b2a0f6199636d975df3f9b45832d0a46
pankajmourya123 <pankajmourya5000@gmail.com>
2023-01-18 23:21:52 +0530

    second commit

--- script.js
+++ script.js
@@ -2,18 +2,20 @@ let cal=document.getElementById("cal")
 let fah=document.getElementById("fah")
 let kel=document.getElementById("kel")
 cal.addEventListener('input',function(){
-    let c = this.value;
+    let c =Number( this.value);
     let f =(c*9/5)+32;
-    let k =c+ 273.15
+    let k = Number( c + 273.15)
+
     if(!Number.isInteger(f)){
         f=f.toFixed(4)
     }
-    fah.value=f;
-    kel.value=k;
+    fah.value = f;
+    kel.value = k;
+   
 })
 
 fah.addEventListener('input',function(){
-    let f = this.value;
+    let f = Number(this.value);
     let c =(f-32)*5/9;
     let k=(32*f - 32) *5/9 + 273.15
     if(!Number.isInteger(c)){
@@ -24,7 +26,7 @@ fah.addEventListener('input',function(){
 }) 
 
 kel.addEventListener('input',function(){
-    let k = this.value;
+    let k = Number(this.value);
     let c =k - 273.15;
     let f=(k - 273.15) * 9/5 + 32
     if(!Number.isInteger(c)){
